d7bfa35038a6f1b39f18ffe302916073a0695c41
jroberto-inv <jroberto@inv.com.vc>
2022-05-11 00:11:00 -0300

    alter state component FeedbackSuccessStepProps in index

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -40,6 +40,7 @@ export function WidgetForm() {
   const [feedbackSent, setFeedbackSent] = useState(false)
 
   function handleRestartFeedback() {
+    setFeedbackSent(false)
     setFeedbackType(null)
   }
 
@@ -47,7 +48,9 @@ export function WidgetForm() {
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
       { feedbackSent ? (
-        <FeedbackSuccessStep />
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       ) : (
         <>
           {! feedbackType ? (
