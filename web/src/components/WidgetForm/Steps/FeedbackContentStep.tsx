0b698aedfbfc5353beb106d84baf38ffc433bfac
jroberto-inv <jroberto@inv.com.vc>
2022-05-10 22:11:57 -0300

    create button for camera in FeedbackContentStep

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 
-import { ArrowLeft } from "phosphor-react"
+import { ArrowLeft, Camera } from "phosphor-react"
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 
@@ -39,10 +39,17 @@ export function FeedbackContentStep({
           placeholder="Conte com detalhes o que está acontecendo"
         />
 
-        <footer className="flex gap mt-2">
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transiction-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+          >
+            <Camera className="w-6 h-6 text-zinc-100" />
+          </button>
+
           <button
             type="submit"
-            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focu:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
           >
             Enviar feedback
           </button>
