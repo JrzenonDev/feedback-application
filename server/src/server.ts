c4b8e54518580e7c88edd8ecee9b6bdb000b2b31
jroberto-inv <jroberto@inv.com.vc>
2022-05-12 13:19:01 -0300

    configure initial sendMail use nodemailer and mailtrap to receive messages tests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 import { prisma } from './prisma'
+import nodemailer from 'nodemailer'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "1f4a67778fa1c6",
+    pass: "9e8e3990b24a36"
+  }
+})
 
 app.post('/feedbacks', async (req, res) => {
 
@@ -16,6 +26,18 @@ app.post('/feedbacks', async (req, res) => {
       comment: comment,
       Screenshot: Screenshot
     }
+  })
+
+  transport.sendMail({
+    from: 'Equipe Feedget <suporte@feedget.com.br>',
+    to: 'José Roberto <jrobertoonb@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo de feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback })
