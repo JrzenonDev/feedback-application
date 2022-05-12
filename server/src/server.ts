4d782a24568b474c8ee4a76aaf2bf8b985a34f50
jroberto-inv <jroberto@inv.com.vc>
2022-05-12 02:26:38 -0300

    create route post for create feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,25 @@
 import express from 'express'
+import { prisma } from './prisma'
 
 const app = express()
+
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+
+  // destructuring
+  const { type, comment, Screenshot } =  req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type: type,
+      comment: comment,
+      Screenshot: Screenshot
+    }
+  })
+
+  return res.status(201).json({ data: feedback })
+})
 
 app.listen(3333, () => {
   console.log('HTTP server running teste!')
