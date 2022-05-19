83a2126d006058895d057330c1a5fcfb86074f99
jroberto-inv <jroberto@inv.com.vc>
2022-05-19 15:22:30 -0300

    covering tests of type submit, without coment and type submit

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,11 +1,13 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
 
 describe('Submit feedback', () => {
+
+  const submitFeedback = new SubmitFeedbackUseCase(
+    { create: async () => {} },
+    { sendMail: async () => {} }
+  )
+
   it('should be able to submit a feedback', async () => {
-    const submitFeedback = new SubmitFeedbackUseCase(
-      { create: async () => {} },
-      { sendMail: async () => {} }
-    )
 
     await expect(submitFeedback.execute({
       type: 'BUG',
@@ -13,4 +15,32 @@ describe('Submit feedback', () => {
       Screenshot: 'data:image/png;base64,dsadasdasdasdasdas'
     })).resolves.not.toThrow()
   })
+
+  it('should not be able to submit feedback whitout type', async () => {
+
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'Example comment',
+      Screenshot: 'data:image/png;base64,dsadasdasdasdasdas'
+    })).rejects.toThrow()
+  })
+
+  it('should not be able to submit feedback whitout comment', async () => {
+
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      Screenshot: 'data:image/png;base64,dsadasdasdasdasdas'
+    })).rejects.toThrow()
+  })
+
+  it('should not be able to submit feedback with an screenshot invalid.', async () => {
+
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'está tudo bugado',
+      Screenshot: 'dsadasd7'
+    })).rejects.toThrow()
+  })
+
 })
--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -17,6 +17,14 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, Screenshot } = request
 
+    if (!type) {
+      throw new Error('Type is required')
+    }
+
+    if (!comment) {
+      throw new Error('Comment is required')
+    }
+
     if (Screenshot && !Screenshot.startsWith('data:image/png;base64')) {
       throw new Error('Invalid screenshot format.')
     }
