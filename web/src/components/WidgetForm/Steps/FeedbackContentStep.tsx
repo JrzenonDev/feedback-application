2dcdf91440f80788780985fd62afe86728aedff5
jroberto-inv <jroberto@inv.com.vc>
2022-05-20 11:50:52 -0300

    create state isSendingFeedback

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,9 +1,9 @@
-
 import { ArrowLeft } from "phosphor-react"
 import { FormEvent, useState } from "react"
 import { FeedbackType, feedbackTypes } from ".."
 import { api } from "../../../lib/api"
 import { CloseButton } from "../../CloseButton"
+import { Loading } from "../../Loading"
 import { ScreenshotButton } from "../ScreenshotButton"
 
 interface FeedbackContentStepProps {
@@ -20,11 +20,14 @@ export function FeedbackContentStep({
 
   const [Screenshot, setScreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
   async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault()
+
+    setIsSendingFeedback(true)
     // console.log({
     //   Screenshot,
     //   comment
@@ -35,6 +38,8 @@ export function FeedbackContentStep({
       comment,
       Screenshot
     })
+
+    setIsSendingFeedback(false)
 
     onFeedbackSent()
   }
@@ -73,10 +78,10 @@ export function FeedbackContentStep({
 
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            { isSendingFeedback ? <Loading /> : 'Enviar feedback' }
           </button>
         </footer>
       </form>
